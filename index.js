c4726a7f7e85c9f2805fb4c072c2fa913dd151db
loren <lorenmichaelh@gmail.com>
2021-11-10 19:00:30 -0600

    commit tree 2d54e9572d32766868072f97df34598b971802f4

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Code your solution in this file!
+const hq = 42;
+
+function distanceFromHqInBlocks(pickup) {
+    if (pickup <= hq) {
+        return distanceFromHqInBlocks = (hq - pickup);
+    } else if (pickup >= hq) {
+        return distanceFromHqInBlocks = (pickup - hq);
+    }
+}
+
+function distanceFromHqInFeet(pickup) {
+    if (pickup <= hq) {
+        distanceFromHqInBlocks = (hq - pickup);
+        return distanceFromHqInFeet = (distanceFromHqInBlocks * 264);
+    } else if (pickup >= hq) {
+        distanceFromHqInBlocks = (pickup - hq);
+        return distanceFromHqInFeet = (distanceFromHqInBlocks * 264);
+    }
+}
+
+function distanceTravelledInFeet(pickup, dropoff) {
+    let distanceTravelledInBlocks = Math.abs(pickup - dropoff);
+    return distanceTravelledInBlocks * 264;
+}
+
+function calculatesFarePrice(start, destination) {
+    let distanceTravelledInFeet = Math.abs((start - destination) * 264);
+    if (distanceTravelledInFeet < 400) {
+        return distanceTravelledInFeet * 0;
+    } else if (distanceTravelledInFeet <= 2000) {
+        return (distanceTravelledInFeet - 400) * .02;
+    } else if (distanceTravelledInFeet < 2500) {
+        return 25;
+    } else if (distanceTravelledInFeet >= 2500) {
+        return 'cannot travel that far'
+    }
+}
